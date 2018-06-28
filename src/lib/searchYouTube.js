06ed8bde32740e600c37beec8c452051ae59f9d0
arobcode2 <andrew-c-rob@live.com>
2018-06-28 17:29:17 -0500

    Start implementation searchYoutube GET request

--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -1,5 +1,13 @@
 var searchYouTube = (options, callback) => {
   // TODO
+  // $.ajax({
+  //   part
+  //   query: options.query,
+  //   data: ,
+  //   success: ,
+  //   type: GET
+
+  // })
 };
 
 window.searchYouTube = searchYouTube;
