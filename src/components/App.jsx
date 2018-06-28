99f99c55bba395f8903cea1c929e9d30b1f7021f
arobcode2 <andrew-c-rob@live.com>
2018-06-28 10:21:48 -0500

    Refactor App into ES6 syntax

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,21 +1,23 @@
-var App = () => (
+class App extends React.Component {
   //this is an App component
-  <div>
-    <nav className="navbar">
-      <div className="col-md-6 offset-md-3">
-        <div><h5><em>search</em></h5></div>
+  render() {
+    return <div>
+      <nav className="navbar">
+        <div className="col-md-6 offset-md-3">
+          <div><h5><em>search</em></h5></div>
+        </div>
+      </nav>
+      <div className="row">
+        <div className="col-md-7">
+          <div><h5><em><VideoPlayer video={exampleVideoData[0]}/></em></h5></div>
+        </div>
+        <div className="col-md-5">
+          <div><h5><em><VideoList videos={exampleVideoData}/></em></h5></div>
+        </div>
       </div>
-    </nav>
-    <div className="row">
-      <div className="col-md-7">
-        <div><h5><em><VideoPlayer video={exampleVideoData[0]}/></em></h5></div>
-      </div>
-      <div className="col-md-5">
-        <div><h5><em><VideoList videos={exampleVideoData}/></em></h5></div>
-      </div>
-    </div>
-  </div>
-);
+    </div>;
+  }
+}
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
