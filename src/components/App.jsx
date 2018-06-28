97faf64253acb3cbedf88cbb156a3b18d4f87d11
arobcode2 <andrew-c-rob@live.com>
2018-06-28 14:50:41 -0500

    implement the connection between the list of videos and video player

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,6 +4,23 @@ class App extends React.Component {
     super(props);
     
     this.state = {index: 0, videos: exampleVideoData};
+
+    this.handleVideoTitleClick = this.handleVideoTitleClick.bind(this);
+  }
+
+  handleVideoTitleClick(e) {
+    //console.log(e.currentTarget.textContent);
+    var count = 0;
+    var videoIndex = 0;
+    for (let video of this.state.videos) {
+      if (video.snippet.title === e.currentTarget.textContent) {
+        videoIndex = count;
+      }
+      count++;
+    }
+    this.setState({
+      index: videoIndex
+    });
   }
 
   render() {
@@ -19,7 +36,7 @@ class App extends React.Component {
           <div><h5><em><VideoPlayer video={this.state.videos[this.state.index]}/></em></h5></div>
         </div>
         <div className="col-md-5">
-          <div><h5><em><VideoList videos={this.state.videos}/></em></h5></div>
+          <div><h5><em><VideoList videos={this.state.videos}cb={this.handleVideoTitleClick}/></em></h5></div>
         </div>
       </div>
     </div>;
--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,7 +1,7 @@
 var VideoList = (props) => (
   <div className="video-list">      
     {props.videos.map( (video, i) => {
-      return <VideoListEntry key={i} video={video}/>;
+      return <VideoListEntry key={i} video={video} cb={props.cb}/>;
     }
     )}
   </div>
